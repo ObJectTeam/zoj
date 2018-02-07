da73ff64e6ccaf2f8b3cf7ae3a58e3759a2576cf
zhangzisu <admin@zhangzisu.cn>
2018-02-07 10:50:07 +0800

    update session

--- app.js
+++ app.js
@@ -122,7 +122,6 @@ global.zoj = {
 		let Session = require('express-session');
 		let FileStore = require('session-file-store')(Session);
 		let sessionConfig = {
-			secret: this.config.session_secret,
 			cookie: {},
 			rolling: true,
 			saveUninitialized: true,
