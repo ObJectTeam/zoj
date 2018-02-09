56e1195fe5c1fbd6e8efc1efa842901c024c7eba
ZhangZisu <admin@zhangzisu.cn>
2018-02-09 20:23:02 +0800

    Update copyright info

--- utility.js
+++ utility.js
@@ -1,20 +1,6 @@
 /*
- *  This file is part of ZOJ.
- *
- *  Copyright (c) 2016 Menci <huanghaorui301@gmail.com>
- *
- *  ZOJ is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Affero General Public License as
- *  published by the Free Software Foundation, either version 3 of the
- *  License, or (at your option) any later version.
- *
- *  ZOJ is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Affero General Public License for more details.
- *
- *  You should have received a copy of the GNU Affero General Public
- *  License along with ZOJ. If not, see <http://www.gnu.org/licenses/>.
+ *  File   : utility.js
+ *  Create : 2018-02-09
  */
 
 'use strict';
